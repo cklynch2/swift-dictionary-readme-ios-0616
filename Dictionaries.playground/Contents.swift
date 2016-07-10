9c8ff83aca83609ff8c8bde575defdaf1b936946
cklynch2 <cklynch2@gmail.com>
2016-07-10 07:29:29 -0400

    Introductory reading about dictionaries complete

--- Dictionaries.playground/Contents.swift
+++ Dictionaries.playground/Contents.swift
@@ -28,3 +28,28 @@ definition = favoriteWords["strategery"]
 
 favoriteWords["strategery"] = nil
 print(favoriteWords)
+
+// Adding an element to a dictionary with subscript syntax:
+favoriteWords["petrichor"] = "smell of rain"
+print(favoriteWords)
+
+// Accessing a value from a dictionary using its key. Note that the return type is an optional, to account for the case of a key that does not exist. Hence this will print Optional("smell of rain")
+let newDefinition = favoriteWords["petrichor"]
+print(newDefinition)
+
+// You can 'uwrap' the string value associated with a given key with optional binding:
+if let newDefinition = favoriteWords["petrichor"] {
+    print("The definition is \(newDefinition).")
+} else {
+    print("This word is not in the dictionary.")
+}
+
+if let missingDefinition = favoriteWords["fleer"] {
+    print("The definition is 'to laugh jeeringly'.")
+} else {
+    print("This word is not in the dictionary.")
+}
+
+// Remove element from the dictionary by reassigning its value to nil:
+favoriteWords["wabbit"] = nil
+print("No time for feeling wabbit, time to greet the day!\n\(favoriteWords)")
